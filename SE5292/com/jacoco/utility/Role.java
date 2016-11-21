48161214819a4af7a31a7d8d7d27bc5f807ae7e0
manjeetsingh87 <manjeets@buffalo.edu>
2016-11-20 19:09:40 -0500

    updated Role.java

--- SE5292/com/jacoco/utility/Role.java
+++ SE5292/com/jacoco/utility/Role.java
@@ -1,7 +1,22 @@
 package com.jacoco.utility;
 
 public enum Role {
-	P, S, M, TL, A
+	Professor("P"),
+	Student("S"),
+	Manager("M"),
+	TechnicalLead("TL"),
+	Admin("A");
 	
+	private String role;
 	
+	Role(String role) {
+		if(null == role || role.length() == 0)
+			throw new IllegalArgumentException("Invalid Role type entered. Role can be one among Professor, Student, Manager or TechnicalLead");
+		
+		this.role = role;
+	}
+	
+	public String roleType() {
+		return role;
+	}
 }
